ea8343c9604e6185acde5c1eda5d7afd2ef80992
Marvin <maassenm3@gmail.com>
2025-09-28 21:29:41 +0200

    Vid 16 - Close Button

--- Plugins/Inventory/Source/Inventory/Public/Inv_PlayerController.h
+++ Plugins/Inventory/Source/Inventory/Public/Inv_PlayerController.h
@@ -23,7 +23,7 @@ public:
 	AInv_PlayerController();
 	virtual void Tick(float DeltaTime) override;
 
-	UFUNCTION(Blueprintable)
+	UFUNCTION(BlueprintCallable, Category="Inventory")
 	void ToggleInventory();
 
 protected:
