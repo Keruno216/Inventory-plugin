ff9ab2a2d33e213d3f2f408c37b5a817ef375164
Marvin <maassenm3@gmail.com>
2025-09-30 20:26:16 +0200

    Vid 30 - Item Fragments

---
+++ Plugins/Inventory/Source/Inventory/Private/Items/Fragments/Inv_FragmentTags.cpp
@@ -0,0 +1,6 @@
+﻿#include "Items/Fragments/Inv_FragmentTags.h"
+
+namespace FragmentTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(GridFragment)
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Items/Fragments/Inv_ItemFragment.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Types/AttributeStorage.h"
+
+#include "Inv_ItemFragment.generated.h"
+
+USTRUCT(BlueprintType)
+struct FInv_ItemFragment
+{
+	GENERATED_BODY()
+
+public:
+	FInv_ItemFragment()
+	{
+	}
+
+	FInv_ItemFragment(const FInv_ItemFragment&) = default;
+	FInv_ItemFragment& operator=(const FInv_ItemFragment&) = default;
+	FInv_ItemFragment(FInv_ItemFragment&&) = default;
+	FInv_ItemFragment& operator=(FInv_ItemFragment&&) = default;
+
+	virtual ~FInv_ItemFragment()
+	{
+	}
+
+	FGameplayTag GetFragmentTag() const { return FragmentTag; }
+	void SetFragmentTag(FGameplayTag Tag) { FragmentTag = Tag; }
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FGameplayTag FragmentTag = FGameplayTag::EmptyTag;
+};
+
+USTRUCT(BlueprintType)
+struct FInv_GridFragment : public FInv_ItemFragment
+{
+	GENERATED_BODY()
+
+public:
+	FIntPoint GetGridSize() const { return GridSize; }
+	void SetGridSize(const FIntPoint& Size) { GridSize; }
+	float GetGridPadding() const { return GridPadding; }
+	void SetGridPadding(float Padding) { GridPadding = Padding; }
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FIntPoint GridSize{1, 1};
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	float GridPadding{0.f};
+};
