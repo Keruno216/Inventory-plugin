a46c053927a521214d3495808a0977de3b5590e2
Marvin <maassenm3@gmail.com>
2025-10-04 04:22:14 +0200

    Vid 49 - Is Index Claimed....................Again

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
@@ -61,7 +61,9 @@ private:
 	                    const TSet<int32>& CheckedIndices,
 	                    TSet<int32>& OutTentativelyClaimed);
 	FIntPoint GetItemDimensions(const FInv_ItemManifest& Manifest) const;
-	bool CheckSlotConstraints(const UInv_GridSlot* SubGridSlot) const;
+	bool CheckSlotConstraints(const UInv_GridSlot* SubGridSlot, const TSet<int32>& CheckedIndices,
+	                          TSet<int32>& OutTentativelyClaimed) const;
+	bool HasValidItem(const UInv_GridSlot* GridSlot) const;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Inventory")
 	EInv_ItemCategory ItemCategory;
