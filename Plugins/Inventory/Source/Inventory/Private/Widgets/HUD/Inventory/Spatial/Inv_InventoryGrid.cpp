5140a4dd748b4d148e521cfbcec8f93154cab132
Marvin <maassenm3@gmail.com>
2025-10-05 03:34:25 +0200

    vid 56 - Filling in the Results

--- Plugins/Inventory/Source/Inventory/Private/Widgets/HUD/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/HUD/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -81,7 +81,6 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 			continue;
 		}
 
-		CheckedIndices.Append(TentativelyClaimed);
 
 		// How much to fill?
 		const int32 AmountToFillInSlot = DetermineFillAmountForSlot(Result.bStackable, MaxStackSize, AmountToFill,
@@ -91,10 +90,28 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 			continue;
 		}
 
-		// Update the amount left to fill
-	}
+		CheckedIndices.Append(TentativelyClaimed);
 
-	// How much is the Remainder?
+		// Update the amount left to fill
+		Result.TotalRoomToFill += AmountToFillInSlot;
+		Result.SlotAvailabilities.Emplace(
+			FInv_SlotAvailability{
+				HasValidItem(GridSlot) ? GridSlot->GetUpperLeftIndex() : GridSlot->GetIndex(),
+				Result.bStackable ? AmountToFillInSlot : 0,
+				HasValidItem(GridSlot)
+			}
+		);
+
+		AmountToFill -= AmountToFillInSlot;
+
+		// How much is the Remainder?
+		Result.Remainder = AmountToFill;
+
+		if (AmountToFill == 0)
+		{
+			return Result;
+		}
+	}
 
 
 	return Result;
